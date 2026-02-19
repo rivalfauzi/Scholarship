e7f2a9aa53bf86b0c4464e8c7408856ae0297fdf
Rival Fauzi <rival.fauzi.id@gmail.com>
2026-02-19 15:54:18 +0700

    initial commit

---
+++ ScholarGo/App/ScholarGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScholarshipsApp.swift
+//  Scholarships
+//
+//  Created by MCO on 19/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ScholarGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppControllerView()
+        }
+    }
+}
